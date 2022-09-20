38d99308e609326c3ded8aa7eb088e7c0b8ffe64
surajbhan-3 <surajbhan2boaz@gmail.com>
2022-09-20 21:10:35 +0530

    prime and palindrome

---
+++ palindrome.js
@@ -0,0 +1,5 @@
+let string ="racecar";
+let bag="";
+for(let i=0; i<=string.length-1; i++){
+      bag=bag+string[i];
+      };
---
+++ prime_number.js
@@ -0,0 +1,15 @@
+let num=13;
+
+for(let i=0; i<=num; i++){
+  if(num%i==2){
+     count++;
+     }
+    }
+     
+     
+    if(count==3){
+console.log("prime")
+}
+else{
+console.log("Not prime");
+
